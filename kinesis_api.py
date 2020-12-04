7c201de4da719d72021c6e3b65b99795fc314a64
Maneesh Divana <maneeshd@MacBook-Pro.cable.rcn.com>
2020-12-04 00:10:01 -0600

    Updated kinesis api

--- kinesis_api.py
+++ kinesis_api.py
@@ -6,6 +6,9 @@ Python: 3.7.9
 AWS Kinesis Stream Producer and Consumer API using boto3
 """
 from os import getenv
+from json import dumps, loads
+from typing import List, Any
+from datetime import datetime, timedelta
 from boto3 import Session as BotoSession
 
 
@@ -42,6 +45,9 @@ class KinesisAPI:
             region_name=self.__region
         )
 
+        # Shard Itertator type to read Records from Kinesis Data Stream
+        self.iterator_type = None
+
         # Get the kinesis client
         self.client = self.session.client("kinesis")
 
@@ -53,49 +59,141 @@ class KinesisAPI:
             self.shard_ids = [shard["ShardId"] for shard in shards]
             _ = self.shard_ids[0]
         except Exception as err:
-            raise ValueError(f"! Failed to get Shard ID's !\n{err}")
+            print("! Failed to get Shard ID's !")
+            raise err
 
     def __str__(self) -> str:
-        return f"<KinesisAPI(stream_name={self.stream_name}, shard_ids={self.shard_ids})>"
+        return f"<KinesisAPI(stream_name='{self.stream_name}', shard_ids={self.shard_ids})>"
 
-    def write_record(self) -> None:
+    def write_record(self, data: Any, partition_key: str) -> dict:
         """
-        Put a record into Kinesis Data Stream
+        Writes a single data record into an Amazon Kinesis data stream
         """
-        pass
+        try:
+            return self.client.put_record(
+                StreamName=self.stream_name,
+                Data=dumps(data),
+                PartitionKey=f"{partition_key}"
+            )
+        except Exception as err:
+            print(f"! Failed to write data to stream: {self.stream_name} !")
+            raise err
 
-    def write_records(self) -> None:
+    def write_records(self, data: List[Any], partition_key: str) -> None:
         """
-        Put a list of records into Kinesis Data Stream
-        Limit: 500 records
+        Writes multiple data records into a Kinesis data stream in a single call
+        Limit: 500 records in 'data'
         """
-        pass
+        if len(data) > 500:
+            raise ValueError("! Number of records in data exceeded limit(500) !")
 
-    def get_shard_iterator(self, iterator_type: str = "TRIM_HORIZON"):
+        records = [
+            {"Data": dumps(ele), "PartitionKey": partition_key} for ele in data
+        ]
+        try:
+            return self.client.put_record(
+                StreamName=self.stream_name,
+                Records=records
+            )
+        except Exception as err:
+            print(f"! Failed to write data to stream: {self.stream_name} !")
+            raise err
+
+    def set_shard_iterator_type(self, iterator_type: str) -> None:
+        self.iterator_type = iterator_type
+
+    def get_shard_iterator(self, iterator_type: str = None, sequence_number: str = None) -> str:
         """
         Get the first shard iterator for get_records
 
         param iterator_type (str): Iterator type
 
+        AFTER_SEQUENCE_NUMBER : Start reading right after the position denoted by a specific
+                                sequence number, provided in the value StartingSequenceNumber
         TRIM_HORIZON          : Start reading at the last untrimmed record in the shard in the
                                 system, which is the oldest data record in the shard.
         LATEST                : Start reading just after the most recent record in the shard,
                                 so that you always read the most recent data in the shard.
         """
+        if not iterator_type:
+            iterator_type = self.iterator_type if self.iterator_type else "TRIM_HORIZON"
+        if iterator_type not in ["AFTER_SEQUENCE_NUMBER", "TRIM_HORIZON", "LATEST"]:
+            iterator_type = "TRIM_HORIZON"
+        if iterator_type == "AFTER_SEQUENCE_NUMBER" and not sequence_number:
+            iterator_type = "TRIM_HORIZON"
+        self.iterator_type = iterator_type
         try:
-            iter_resp = self.client.get_shard_iterator(
-                StreamName=self.stream_name,
-                ShardId=self.shard_ids[0],
-                ShardIteratorType=iterator_type
-            )
+            if iterator_type == "AFTER_SEQUENCE_NUMBER":
+                iter_resp = self.client.get_shard_iterator(
+                    StreamName=self.stream_name,
+                    ShardId=self.shard_ids[0],
+                    ShardIteratorType=iterator_type,
+                    StartingSequenceNumber=sequence_number
+                )
+            else:
+                iter_resp = self.client.get_shard_iterator(
+                    StreamName=self.stream_name,
+                    ShardId=self.shard_ids[0],
+                    ShardIteratorType=iterator_type
+                )
             iter_resp["ShardIterator"]
         except Exception as err:
-            raise ValueError(f"! Failed to get Shard Iterator !\n{err}")
+            print(f"! Failed to get Shard Iterator for stream: {self.stream_name} !")
+            raise err
         else:
             return iter_resp["ShardIterator"]
 
-    def read_records():
-        pass
+    def read_records(
+        self,
+        time_limit: float,
+        shard_iterator: str = None,
+        records_limit: int = 5000,
+        debug: bool = False
+    ) -> dict:
+        """
+        Gets data records from a Kinesis data stream's shard.
+
+        time_limit: Time in MINUTES to keep scanning for records
+        """
+        # Calculate end time
+        end_time = datetime.now() + timedelta(minutes=time_limit)
+
+        if not shard_iterator:
+            shard_iterator = self.get_shard_iterator()
+
+        while True:
+            try:
+                # Get data
+                record_resp = self.client.get_records(
+                    ShardIterator=shard_iterator,
+                    Limit=records_limit
+                )
+                # Only run for a certain amount of time.
+                # Stop looping if no data returned. This means it's done
+                now = datetime.now()
+
+                if debug:
+                    print("")
+                    print(f"[read_records] ShardIterator: {shard_iterator}")
+                    print(f"[read_records][{now.strftime('%Y/%m/%d %H:%M:%S')}] {record_resp}")
+                    print("")
+
+                if end_time < now or not record_resp:
+                    break
+                # yield data to outside calling iterator
+                for record in record_resp["Records"]:
+                    sequence_number = record["SequenceNumber"]
+                    try:
+                        yield {"data": loads(record["Data"]), "sequence_number": sequence_number}
+                    except Exception as err:
+                        print(f"[WARN] Error deserializing record's data: {err}")
+                        yield {"data": record["Data"], "sequence_number": sequence_number}
+                # Get next iterator for shard from previous request
+                shard_iterator = record_resp["NextShardIterator"]
+            except Exception as err:
+                print(f"! Error getting records from stream: {self.stream_name} !")
+                print(err, "\n")
+                break
 
     def __del__(self):
         self.__access_key_id = None
@@ -109,3 +207,20 @@ if __name__ == "__main__":
     print("TEST")
     api = KinesisAPI("stock-stream")
     print(api)
+
+    # api.set_shard_iterator_type("LATEST")
+    # Default Shard Iterator Type: TRIM_HORIZON
+    for record in api.read_records(time_limit=1.0):
+        data = record.get("data")
+        last_seq_num = record.get("sequence_number")
+        print(f"\nDATA: {data}\nLAST_SEQ_NUM: {last_seq_num}\n")
+
+    # Use AFTER_SEQUENCE_NUMBER to only read data after last read
+    # shard_iter = api.get_shard_iterator(
+    #     iterator_type="AFTER_SEQUENCE_NUMBER",
+    #     sequence_number="49613271809655507031116572764155274724471449482516496386"
+    # )
+    # for record in api.read_records(time_limit=1.0, shard_iterator=shard_iter):
+    #     data = record.get("data")
+    #     last_seq_num = record.get("sequence_number")
+    #     print(f"\nDATA: {data}\nLAST_SEQ_NUM: {last_seq_num}\n")
