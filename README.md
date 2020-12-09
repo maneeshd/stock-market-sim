d586dd9853f1228f9aaeecb643a169c103f3dcc7
Yashas Reddy <yashasycsc@outlook.com>
2020-12-09 10:08:20 +0530

    consumer setup

--- README.md
+++ README.md
@@ -2,9 +2,16 @@
 
 ## Stock streaming and price monitoring system.
 
+### Installing libraries
+
+`$ python -m venv env`
+`$ source env/bin/activate`
+`$ pip install -r requirements.txt`
 
 ### Setting AWS Access Keys in Environment Variables
 
 ```bash
 $ source set_aws_env.sh "<AWS Access Key ID>" "<AWS Secret Access Key>" "<AWS Region>"
 ```
+
+### Create a Kinesis stream on AWS named 'stock-stream' using GUI
--- kinesis/consumer.py
+++ kinesis/consumer.py
@@ -5,3 +5,19 @@ Python: 3.7.9
 
 Consumer for AWS Kinesis Stock Data Stream
 """
+from kinesis_api import KinesisAPI
+
+KINESIS_STREAM_NAME = "stock-stream"
+KINESIS_SHARD_PARTITION_KEY = "stock"
+
+
+def consume():
+    api = KinesisAPI(stream_name=KINESIS_STREAM_NAME)
+    pass
+
+
+if __name__ == "__main__":
+    print("====================================")
+    print("Stock Data Consumer for AWS Kinesis")
+    print("====================================")
+    consume()
