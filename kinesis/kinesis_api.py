b28b75c64fd1e202baa1352925fa3ea37cc630a1
Maneesh Divana <maneeshd@MacBook-Pro.local>
2020-12-09 17:53:53 +0530

    Added DynamoDB API

--- kinesis/kinesis_api.py
+++ kinesis/kinesis_api.py
@@ -8,7 +8,7 @@ AWS Kinesis Stream Producer and Consumer API using boto3
 from os import getenv
 from json import dumps, loads
 from typing import List, Any
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date, time
 from boto3 import Session as BotoSession
 
 
@@ -221,17 +221,153 @@ class KinesisAPI:
         self.session = None
 
 
+class DynamoDbAPI():
+    def __init__(self, table_name: str):
+        if not table_name:
+            raise ValueError("! DynamoDB Table Name is requied !")
+        self.table_name = table_name
+
+        # Get AWS Access Keys from Environment Varibales
+        self.__access_key_id = getenv("AWS_ACCESS_KEY_ID")
+        self.__secret_access_key = getenv("AWS_SECRET_ACCESS_KEY")
+        self.__region = getenv("AWS_REGION_NAME")
+
+        if self.__access_key_id is None:
+            raise ValueError(
+                "! AWS_ACCESS_KEY_ID was not found/not set in environment variables. !"
+            )
+
+        if self.__secret_access_key is None:
+            raise ValueError(
+                "! AWS_SECRET_ACCESS_KEY was not found/not set in environment variables. !"
+            )
+
+        if self.__region is None:
+            raise ValueError(
+                "! AWS_REGION_NAME was not found/not set in environment variables. !"
+            )
+
+        # Creating a boto3 session, connect to AWS
+        self.session = BotoSession(
+            aws_access_key_id=self.__access_key_id,
+            aws_secret_access_key=self.__secret_access_key,
+            region_name=self.__region
+        )
+
+        # Create a DynamoDB client from session
+        self.db = self.session.client("dynamodb")
+
+        # Check if table exists
+        assert self.table_name in self.db.list_tables().get("TableNames"), (
+            f"Table: '{self.table_name}' not found in DynamoDB"
+        )
+
+    def __str__(self) -> str:
+        return f"<DynamoDbAPI(table_name='{self.table_name}')>"
+
+    def __get_mapped_data(self, data) -> dict:
+        if isinstance(data, (str, date, time, datetime)):
+            return {"S": str(data)}
+        elif isinstance(data, (int, float)):
+            return {"N": str(data)}
+        elif isinstance(data, bytes):
+            return {"B": data}
+        elif isinstance(data, list):
+            if all([isinstance(ele, str) for ele in data]):
+                # list of str
+                return {"SS": data}
+            elif all([isinstance(ele, (int, float)) for ele in data]):
+                return {"NS": [str(ele) for ele in data]}
+            else:
+                return {"S": str(data)}
+        else:
+            return {"S": str(data)}
+
+    def prepare_item(self, data: dict) -> dict:
+        """
+        Prepare the Item object to put into DynamoDB
+        """
+        item = {}
+        if not data:
+            return
+        for key, val in data.items():
+            item[key] = self.__get_mapped_data(val)
+        return item
+
+    def put(self, data: dict) -> dict:
+        """
+        Put data into DynamoDB Table
+        """
+        item = self.prepare_item(data)
+        resp = self.db.put_item(TableName=self.table_name, Item=item)
+        resp = {}
+        return {
+            "request_id": resp.get("ResponseMetadata", {}).get("RequestId"),
+            "status_code": resp.get("ResponseMetadata", {}).get("HTTPStatusCode")
+        }
+
+    def get(self, key: dict) -> dict:
+        """
+        Get item for a particular key from DynamoDB table
+
+        key is a dict with Partition Key and its Value.
+        {"symbol": "AAPL"} -> when there is no sort key
+
+        For composite keys (When there is a sort key)
+        Have to use this: {"symbol": "AAPL", "minute": "09:32"}
+        """
+        item_key = self.prepare_item(key)
+        resp = self.db.get_item(TableName=self.table_name, Key=item_key)
+        return {
+            "request_id": resp.get("ResponseMetadata", {}).get("RequestId"),
+            "status_code": resp.get("ResponseMetadata", {}).get("HTTPStatusCode"),
+            "item": resp.get("Item")
+        }
+
+    def get_all(self, filter: str = None, expr_attr_values: dict = None) -> dict:
+        """
+        Get all items from DynamoDB table
+        Limit rows using filter string
+
+        filter = "<colum/key> <condition> :<value>"
+
+        Ex:
+        filter = "symbol = :aapl"
+
+        # ExpressionAttributeValues
+        expr_attr_vals = {":aapl": "AAPL"}
+        """
+        resp = {}
+        if filter and expr_attr_values:
+            expr_attr_map = {}
+            for key, value in expr_attr_values.items():
+                expr_attr_map[key] = self.__get_mapped_data(value)
+            print("expr_attr_map:", expr_attr_map)
+            resp = self.db.scan(
+                TableName=self.table_name,
+                FilterExpression=filter,
+                ExpressionAttributeValues=expr_attr_map
+            )
+        else:
+            resp = self.db.scan(TableName=self.table_name)
+        return {
+            "request_id": resp.get("ResponseMetadata", {}).get("RequestId"),
+            "status_code": resp.get("ResponseMetadata", {}).get("HTTPStatusCode"),
+            "items": resp.get("Items")
+        }
+
+
 if __name__ == "__main__":
     print("TEST")
-    api = KinesisAPI("stock-stream")
-    print(api)
+    # api = KinesisAPI("stock-stream")
+    # print(api)
 
     # api.set_shard_iterator_type("LATEST")
     # Default Shard Iterator Type: TRIM_HORIZON
-    for record in api.read_records(time_limit=1.0):
-        data = record.get("data")
-        last_seq_num = record.get("sequence_number")
-        print(f"\nDATA: {data}\nLAST_SEQ_NUM: {last_seq_num}\n")
+    # for record in api.read_records(time_limit=1.0):
+    #     data = record.get("data")
+    #     last_seq_num = record.get("sequence_number")
+    #     print(f"\nDATA: {data}\nLAST_SEQ_NUM: {last_seq_num}\n")
 
     # Use AFTER_SEQUENCE_NUMBER to only read data after last read
     # shard_iter = api.get_shard_iterator(
@@ -242,3 +378,45 @@ if __name__ == "__main__":
     #     data = record.get("data")
     #     last_seq_num = record.get("sequence_number") # or api.last_sequence_number
     #     print(f"\nDATA: {data}\nLAST_SEQ_NUM: {last_seq_num}\n")
+
+    # --------------------------------------------------------------------------------
+
+    # DynamoDB API
+    db_api = DynamoDbAPI("stock-stream-data")
+    print(db_api)
+
+    # data = {
+    #     "minute": "09:31",
+    #     "symbol": "AAPL",
+    #     "open": 117.42,
+    #     "high": 117.825,
+    #     "low": 117.4,
+    #     'close': 117.825,
+    #     "volume": 16300,
+    # }
+
+    # columns = ["minute", "symbol", "open", "high", "low", "close", "volume"]
+
+    # import pandas as pd
+    # df = pd.read_csv("../data/intraday-22-oct-merged.csv")
+
+    # rows = df.head(50)[columns]
+
+    # from time import sleep
+
+    # Put data into db
+    # for idx, row in rows.iterrows():
+    #     print(db_api.put(row.to_dict()))
+    #     sleep(0.5)
+
+    # Read data from db
+    # print(db_api.get({
+    #     "symbol": "AAPL",
+    #     "minute": "09:30"
+    # }))
+
+    # Readd all data from db table
+    # print(db_api.get_all(
+    #     filter="symbol = :aapl",
+    #     expr_attr_values={":aapl": "AAPL"}
+    # ))
